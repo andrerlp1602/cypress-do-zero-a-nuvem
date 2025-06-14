5f6eb41d15d7b9f1fbd5dddee26da0dcb6f8f672
Andre Roberto <andrerlp1602@gmail.com>
2025-06-14 11:33:25 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-
+  projectId: 'tn42ty',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
